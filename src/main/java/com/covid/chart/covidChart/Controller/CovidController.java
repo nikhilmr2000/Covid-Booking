733d47cf15ebbad52bd800bd1cfbe5abf4f7138d
M.R.Nikhil <60879899+nikhilmr2000@users.noreply.github.com>
2022-07-15 00:36:45 +0530

    Update CovidController.java

--- src/main/java/com/covid/chart/covidChart/Controller/CovidController.java
+++ src/main/java/com/covid/chart/covidChart/Controller/CovidController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.CrossOrigin;
 import com.covid.chart.covidChart.Entity.CompleteReceipt;
 import com.covid.chart.covidChart.Entity.Details;
 import com.covid.chart.covidChart.Entity.Hospital;
